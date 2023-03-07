bbacab4be55c2944d0d37e3c727e721974ae151a
임메오 <90150969+Seul-Seul@users.noreply.github.com>
2023-03-08 03:30:21 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.03 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -3,9 +3,7 @@ class Solution {
         boolean answer = true;
         if(s.length() != 4 && s.length() != 6)  return false;
         for(char c : s.toCharArray()){
-            if(!Character.isDigit(c)){
-                return false;
-            }
+            if(!Character.isDigit(c))   return false;
         }
         return answer;
     }
